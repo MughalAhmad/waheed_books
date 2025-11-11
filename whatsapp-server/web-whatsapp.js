e8ae6ea5f6f1e9edc93855f2f554950923676b3d
Faisal Saleem <fysalsaleem@gmail.com>
2025-11-11 18:40:31 +0500

    add correct url

--- whatsapp-server/web-whatsapp.js
+++ whatsapp-server/web-whatsapp.js
@@ -31,7 +31,7 @@ client.on("message", async (msg) => {
 
   await client.sendMessage(
   msg.from, 
-  "Thanks for messaging! I am a bot 🤖\nCheck out this link: https://waheed-books.vercel.app/public/whatsappform"
+  "Thanks for messaging! I am a bot 🤖\nCheck out this link: https://waheed-books-1mqa.vercel.app/public/whatsappform"
 );
 
 });
